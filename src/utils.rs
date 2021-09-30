3894b7dd17db4a804a1014556a94ec5a3439b9a9
kazuki0824 <kazukuro0824@gmail.com>
2021-09-30 21:55:42 +0900

    Add a new transformer

--- src/utils.rs
+++ src/utils.rs
@@ -2,12 +2,12 @@ extern crate byteorder;
 
 use std::io::Cursor;
 
-use byteorder::BigEndian;
+use crate::access_control::types::{EmmExtendedKeys, EmmReceivingKeyPair, WorkingKey};
+use byteorder::{BigEndian, LittleEndian};
 use byteorder::{ReadBytesExt, WriteBytesExt};
-use crate::access_control::types::WorkingKey;
 
 mod cryptography00;
-
+mod cryptography40;
 
 pub(crate) struct BlockConversionSolver00 {
     key: ([u32; 4], [u32; 4]),
@@ -25,44 +25,44 @@ impl BlockConversionSolver00 {
         }
     }
     pub(crate) fn convert(&self, input: Vec<u8>, id: u8) -> Vec<u8> {
-        let mut buf = vec![self.iv];
-
-        //store u64
-        let count = input.len() / 8;
-        let mut pos = Cursor::new(input);
-
-        let mut last_aligned_part: u64 = 0;
-        for _x in 0..count {
-            last_aligned_part = pos.read_u64::<BigEndian>().unwrap();
-            buf.push(last_aligned_part);
-        }
-        let bottom = &pos.into_inner()[8 * count..];
-        //let bottom = pos.remaining_slice();
+        let (aligned, bottom) = {
+            let mut buf = vec![self.iv];
+            //store u64
+            let count = input.len() / 8;
+            let mut pos = Cursor::new(input);
+            for _x in 0..count {
+                buf.push(pos.read_u64::<BigEndian>().unwrap());
+            }
+            let leftover_pos = pos.position() as usize;
+            (buf, pos.into_inner().split_off(leftover_pos))
+            //(buf, pos.remaining_slice())
+        };
 
         //CBC
         let key = if id % 2 == 0 { self.key.0 } else { self.key.1 };
-        let coded: Vec<u64> = buf
+        let coded: Vec<u64> = aligned
             .iter()
             .map(|x| cryptography00::crypto_block00(*x, key, false))
             .collect();
-        for x in 1..buf.len() {
-            buf[x - 1] ^= coded.get(x).unwrap();
-        }
 
-        //re-store u8
-        let out = Vec::<u8>::new();
-        let mut c = Cursor::new(out);
-        let count = buf.len() - 1;
-        for x in 0..count {
-            c.write_u64::<BigEndian>(buf[x]).unwrap();
-        }
+        //re-store u8, and emit the bottom of the aligned part
+        let (mut out, last) = {
+            let mut last_aligned_part = 0;
+            let mut c = Cursor::new(Vec::<u8>::new());
+            for x in 1..aligned.len() {
+                //Chain
+                last_aligned_part = aligned[x];
+                let xor = aligned[x - 1] ^ coded.get(x).unwrap();
+                c.write_u64::<BigEndian>(xor).unwrap();
+            }
+            (c.into_inner(), last_aligned_part)
+        };
 
         //resolve unaligned stubs
         let tail = unsafe {
-            let tail = cryptography00::crypto_block00(last_aligned_part, key, true);
+            let tail = cryptography00::crypto_block00(last, key, true);
             std::mem::transmute::<u64, [u8; 8]>(tail)
         };
-        let mut out = c.into_inner();
         for x in 0..bottom.len() {
             out.push(bottom[x] ^ tail[7 - x])
         }
@@ -70,16 +70,71 @@ impl BlockConversionSolver00 {
     }
 }
 
+pub(crate) struct BlockConversionSolver40 {
+    pub ex_key: EmmExtendedKeys,
+    iv: u64,
+}
+impl BlockConversionSolver40 {
+    pub(crate) fn new(key: EmmReceivingKeyPair) -> Self {
+        Self {
+            ex_key: EmmExtendedKeys {
+                0: key.card_id,
+                1: cryptography40::key_schedule40(key.key),
+            },
+            iv: 0x11096919991927fe,
+        }
+    }
+    pub(crate) fn try_convert(&self, input: Vec<u8>, protocol: u8) -> Option<Vec<u8>> {
+        let (aligned, bottom) = {
+            let mut buf = vec![self.iv];
+            //store u64
+            let count = input.len() / 8;
+            let mut pos = Cursor::new(input);
+            for _x in 0..count {
+                buf.push(pos.read_u64::<LittleEndian>().unwrap());
+            }
+            let leftover_pos = pos.position() as usize;
+            (buf, pos.into_inner().split_off(leftover_pos))
+            //(buf, pos.remaining_slice())
+        };
 
-pub(crate) struct BlockConversionSolver40
-{
+        //CBC
+        let key = self.ex_key.1;
+        let coded: Vec<u64> = aligned
+            .iter()
+            .map(|x| cryptography40::crypto_block_40(*x, key, false, protocol))
+            .collect();
 
+        //re-store u8, and emit the bottom of the aligned part
+        let (mut out, last) = {
+            let mut last_aligned_part = 0;
+            let mut c = Cursor::new(Vec::<u8>::new());
+            for x in 1..aligned.len() {
+                //Chain
+                last_aligned_part = aligned[x];
+                let xor = aligned[x - 1] ^ coded.get(x).unwrap();
+                c.write_u64::<LittleEndian>(xor).unwrap();
+            }
+            (c.into_inner(), last_aligned_part)
+        };
+
+        //resolve unaligned stubs
+        let tail = unsafe {
+            let tail = cryptography40::crypto_block_40(last, key, true, protocol);
+            std::mem::transmute::<u64, [u8; 8]>(tail)
+        };
+        for x in 0..bottom.len() {
+            out.push(bottom[x] ^ tail[7 - x])
+        }
+        Some(out)
+        //TODO: 1 Implement tests, 2 Generate MAC to check it
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::utils::BlockConversionSolver00;
     use crate::access_control::types::WorkingKey;
+    use crate::utils::BlockConversionSolver00;
 
     #[test]
     fn cbc() {
