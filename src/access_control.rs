fea7cc9077d5834b0ecd579a21c54e6707b033bb
kazuki0824 <kazukuro0824@gmail.com>
2021-09-30 21:58:47 +0900

    EMMVerifier

--- src/access_control.rs
+++ src/access_control.rs
@@ -1,7 +1,8 @@
-use std::cell::Cell;
-use crate::access_control::types::{WorkingKey, EmmBody, EmmReceivingKeys};
-use std::sync::mpsc::{Sender, Receiver};
+use crate::access_control::types::{EmmBody, EmmDecryptedBody, EmmReceivingKeyPair, WorkingKey};
 use crate::utils::BlockConversionSolver40;
+use byteorder::{BigEndian, ByteOrder};
+use std::cell::Cell;
+use std::sync::mpsc::{Receiver, Sender};
 
 mod ffi;
 pub mod types;
@@ -10,16 +11,36 @@ pub(crate) struct EcmKeyHolder {
     pub(crate) key_pair: Cell<WorkingKey>,
 }
 
-pub struct EmmVerifier
-{
-    inner: EmmReceivingKeys,
-    inner_extended: BlockConversionSolver40
+pub struct EmmVerifier {
+    inner_extended: Vec<BlockConversionSolver40>,
 }
-impl EmmVerifier
-{
-    pub fn new(keys: EmmReceivingKeys) -> Self
-    {
-        unimplemented!("")
+impl EmmVerifier {
+    pub fn new(keys: Vec<EmmReceivingKeyPair>) -> Self {
+        Self {
+            inner_extended: keys
+                .into_iter()
+                .map(|k| BlockConversionSolver40::new(k))
+                .collect(),
+        }
+    }
+    pub fn try_all_keys(&self, emm: EmmBody) -> Option<EmmDecryptedBody> {
+        for b in &self.inner_extended {
+            if emm.card_id == b.ex_key.0 {
+                if let Some(result) = b.try_convert(emm.info.clone(), emm.protocol) {
+                    return Some(EmmDecryptedBody {
+                        card_id: emm.card_id,
+                        protocol: emm.protocol,
+                        broadcaster_group_id: result[0],
+                        update_number: BigEndian::read_u16(&result[1..=2]),
+                        expiration_date: BigEndian::read_u16(&result[3..=4]),
+                        info: result[5..].into(),
+                    });
+                } else {
+                    continue;
+                }
+            }
+        }
+        return None;
     }
 }
 
