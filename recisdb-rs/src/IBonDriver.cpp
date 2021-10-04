7f5225d143e71f6d6cb028ffc3d9fb5bf9e2f0e4
kazuki0824 <kazukuro0824@gmail.com>
2021-10-04 19:28:17 +0900

    IBonのdynamic_cast周りを整理 create() -> create_interface()

--- recisdb-rs/src/IBonDriver.cpp
+++ recisdb-rs/src/IBonDriver.cpp
@@ -13,4 +13,12 @@ extern "C" {
     {
         return dynamic_cast<IBonDriver3*>(i);
     }
+    const IBonDriver2* interface_check_2_const(const IBonDriver * i)
+    {
+        return dynamic_cast<const IBonDriver2*>(i);
+    }
+    const IBonDriver3* interface_check_3_const(const IBonDriver2 * i)
+    {
+        return dynamic_cast<const IBonDriver3*>(i);
+    }
 }
