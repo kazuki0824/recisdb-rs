c201fedc1a5391474fffb6861252550220e2b9db
kazuki0824 <kazukuro0824@gmail.com>
2022-07-09 20:50:05 +0900

    Enable SIMD in MULTI2 decryption

--- b25-sys/build.rs
+++ b25-sys/build.rs
@@ -12,7 +12,7 @@ fn main() {
     pc.probe("libpcsclite");
     if pc.target_supported() && !(cfg!(target_os = "windows")) {
         println!("cargo:rustc-link-lib=dylib=stdc++");
-        if pc.probe("libarib25").is_err() {
+        if pc.probe("libaribb25").is_err() {
             //start self build
             let mut cm = cmake::Config::new("./externals/libaribb25");
             let res = cm.build();
@@ -23,13 +23,15 @@ fn main() {
         let mut cm = cmake::Config::new("./externals/libaribb25");
         cm.very_verbose(true);
         cm.configure_arg("-DUSE_AVX2=ON");
-        //MSVC + b25-rs(debug) + libarib25(debug) = fail
-        //warning LNK4098: defaultlib \'MSVCRTD.../NODEFAULTLIB:library...
+
+        /*
+        MSVC + libarib25(debug) = fail
+        warning LNK4098: defaultlib \'MSVCRTD.../NODEFAULTLIB:library...
+         */
         cm.profile("Release");
+
         let res = cm.build();
         println!("cargo:rustc-link-search=native={}/lib", res.display());
-        /* MSVC emits two different *.lib files, libarib25.lib and arib25.lib.
-        The first one is a static library, but the other is an import library, which doesn't have any implemation. */
         println!("cargo:rustc-link-lib=dylib=winscard");
     }
     println!("cargo:rustc-link-lib=static=aribb25");
