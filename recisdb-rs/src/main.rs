389e6c07e0bd5fa3e1b8a948d456a22c5e0473a8
maleicacid <maleicacid824+dev@gmail.com>
2023-07-14 01:22:13 +0900

    Remove handle_tuning_error

--- recisdb-rs/src/main.rs
+++ recisdb-rs/src/main.rs
@@ -2,7 +2,7 @@ use crate::utils::StreamExitType;
 use clap::Parser;
 use futures_executor::block_on;
 use futures_time::future::FutureExt;
-use log::{error, info};
+use log::info;
 
 mod channels;
 mod commands;
@@ -11,45 +11,6 @@ mod io;
 mod tuner;
 mod utils;
 
-#[cfg(target_os = "linux")]
-fn handle_tuning_error(e: Box<dyn std::error::Error>) -> ! {
-    if let Some(nix_err) = e.downcast_ref::<nix::Error>() {
-        let current_errno = nix::errno::Errno::from_i32(nix::errno::errno());
-        match current_errno {
-            nix::errno::Errno::EAGAIN => {
-                error!("Channel selection failed. The channel may not be received.");
-            }
-            nix::errno::Errno::EINVAL => {
-                error!("The specified channel is invalid.");
-            }
-            _ => {
-                error!("Unexpected Linux error: {}", nix_err);
-            }
-        }
-    } else if let Some(io_error) = e.downcast_ref::<std::io::Error>() {
-        if let Some(raw_os_error) = io_error.raw_os_error() {
-            match raw_os_error {
-                libc::EALREADY => {
-                    error!("The tuner device is already in use.");
-                }
-                _ => {
-                    error!("Unexpected IO error: {}", io_error);
-                }
-            }
-        } else {
-            error!("Unexpected IO error: {}", io_error);
-        }
-    } else {
-        error!("Unexpected error: {}", e);
-    }
-    std::process::exit(1);
-}
-
-#[cfg(target_os = "windows")]
-fn handle_tuning_error(e: Box<dyn std::error::Error>) -> ! {
-    error!("Unexpected error: {}", e);
-    std::process::exit(1);
-}
 
 fn main() {
     let arg = context::Cli::parse();
