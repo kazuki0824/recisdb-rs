7d58e507ec9d18e9c070d549c09ed6a6dcc5f6e4
tsukumi <tsukumijima@users.noreply.github.com>
2023-08-10 01:16:19 +0000

    Improved log output format by displaying log timestamps in local time

--- recisdb-rs/src/utils.rs
+++ recisdb-rs/src/utils.rs
@@ -1,4 +1,8 @@
-use env_logger::Env;
+use chrono::Local;
+use colored::*;
+use env_logger::{Builder, Env};
+use log::{Level, info};
+use std::io::Write;
 
 pub(crate) enum StreamExitType {
     Success(u64),
@@ -8,5 +12,18 @@ pub(crate) enum StreamExitType {
 }
 
 pub(crate) fn initialize_logger() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    Builder::from_env(Env::default().default_filter_or("info"))
+        .format(|buf, record| {
+            let local_time = Local::now().format("%Y/%m/%d %H:%M:%S");
+            let level = match record.level() {
+                Level::Error => "ERROR:   ".red(),
+                Level::Warn  => "WARNING: ".yellow(),
+                Level::Info  => "INFO:    ".green(),
+                Level::Debug => "DEBUG:   ".cyan(),
+                Level::Trace => "TRACE:   ".blue(),
+            };
+            writeln!(buf, "[{}] {} {}", local_time, level, record.args())
+        })
+        .init();
+    info!("recisdb version {}", env!("CARGO_PKG_VERSION"));
 }
